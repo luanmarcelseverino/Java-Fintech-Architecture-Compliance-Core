c128fba93047237e4f9954cf9b14486a89f6a3fd
Luan <luanmarcel.severino@gmail.com>
2026-04-22 19:14:38 -0300

    ajuste diversos

--- src/main/java/Transacao.java
+++ src/main/java/Transacao.java
@@ -1,14 +1,12 @@
-import lombok.Data;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.Data;
 import java.time.LocalDateTime;
 
-@Data // Cria os Getters (getTipo, getValor, etc)
-@AllArgsConstructor // Cria o construtor (Data, Tipo, Valor, Descrição)
-@NoArgsConstructor
+@Data // Cria todos os Getters, Setters e toString automaticamente
+@AllArgsConstructor // Cria o construtor com os 4 argumentos que a Conta está pedindo
 public class Transacao {
     private LocalDateTime data;
     private String tipo;
     private double valor;
-    private String descricao;
+    private String descricao; // ou o nome que você usou para o 4º argumento
 }
